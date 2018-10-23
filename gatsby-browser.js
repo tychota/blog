cb8eda11453cb94f0d2c7aef199092e6d8160f87
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2018-10-23 20:16:56 +0200

    fix: move the style to gatsby-browser

---
+++ gatsby-browser.js
@@ -0,0 +1,7 @@
+import "bulma/css/bulma.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+
+import "./src/style.css";
+
+import "prismjs/themes/prism-solarizedlight.css";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,4 @@
 // @flow
-
-import "bulma/css/bulma.css";
-import "@fortawesome/fontawesome-free/css/all.css";
-import "../style.css";
-import "prismjs/themes/prism-solarizedlight.css";
-// layouts/index.js
-import "prismjs/plugins/line-numbers/prism-line-numbers.css";
-
 import React from "react";
 import Header from "components/Header";
 import PostsList from "components/PostsList";
