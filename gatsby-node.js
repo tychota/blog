803a1df2f6d61fb178e9435b150b5a42f6531895
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2018-10-24 19:58:44 +0200

    fix: add missing slug field on build

--- gatsby-node.js
+++ gatsby-node.js
@@ -87,3 +87,24 @@ exports.createPages = ({ graphql, actions }) => {
     });
   });
 };
+
+// Add custom url pathname for blog posts.
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
+
+  if (node.internal.type === "File") {
+    const parsedFilePath = path.parse(node.absolutePath);
+    const slug = `/${parsedFilePath.dir.split("---")[1]}/`;
+    createNodeField({ node, name: "slug", value: slug });
+  } else if (
+    node.internal.type === "MarkdownRemark" &&
+    typeof node.slug === "undefined"
+  ) {
+    const fileNode = getNode(node.parent);
+    createNodeField({
+      node,
+      name: "slug",
+      value: fileNode.fields.slug
+    });
+  }
+};
